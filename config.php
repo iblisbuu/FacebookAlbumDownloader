c47ff30fefd5bde61052f85e30850d9536192c10
dhruv vadodariya <dhruvvadodariya03@gmail.com>
2018-12-04 20:51:52 +0530

    update
    te

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 	use Facebook\Facebook;
 	require_once "Facebook/autoload.php";
 	$FB = new \Facebook\Facebook([
-		'app_id' => '523737744738961',
-		'app_secret' => 'fbdcf27c78b4f46a1be153b2e86e39d1',
+		'app_id' => '',
+		'app_secret' => '',
 		'default_graph_version' => 'v3.1',
 		'default_access_token' => 'APP-ID|APP-SECRET'
 	]);
